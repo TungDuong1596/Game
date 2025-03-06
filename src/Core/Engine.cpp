9c4f6a36dac411314f6c5c0cacae827a79ffbaee
tungduong <duongden1509@gmail.com>
2025-03-07 02:32:25 +0700

    WindowAndRenderPractise

--- src/Core/Engine.cpp
+++ src/Core/Engine.cpp
@@ -3,33 +3,54 @@
 
 Engine* Engine::s_Instance = nullptr;
 
-bool Engine::Init()
-{
-    m_IsRunning = true;
+bool Engine::Init(){
+    if(SDL_Init(SDL_INIT_VIDEO)!=0 && IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG)!= 0){
+        SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
+        return false;
+    }
+
+    m_Window = SDL_CreateWindow("Soft Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_HEIGHT, SCREEN_HEIGHT, 0);
+    if(m_Window == nullptr){
+        SDL_Log("%s", SDL_GetError());
+        return false;
+    }
+
+    m_Renderer = SDL_CreateRenderer(m_Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if(m_Renderer == nullptr){
+        SDL_Log("%s", SDL_GetError());
+        return false;
+    }
+
+    return m_IsRunning = true;
 }
 
-bool Engine::Clean()
-{
+void Engine::Update(){
 
 }
 
-void Engine::Quit()
-{
+void Engine::Render(){
+    SDL_SetRenderDrawColor(m_Renderer, 124, 218, 254, 255);
+    SDL_RenderPresent(m_Renderer);
+}
+
+void Engine::Events(){
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch(event.type){
+        case SDL_QUIT:
+            Quit();
+            break;
+    }
+}
+
+bool Engine::Clean(){
 
 }
 
-void Engine::Update()
-{
-    std::cout << "Updating..." << std::endl;
+void Engine::Quit(){
+    m_IsRunning = false;
 }
 
-void Engine::Render()
-{
 
-}
 
-void Engine::Events()
-{
-
-}
 
--- src/Core/Engine.h
+++ src/Core/Engine.h
@@ -1,6 +1,11 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
+#include "SDL.h"
+#include "SDL_image.h"
+
+#define SCREEN_WIDTH 960
+#define SCREEN_HEIGHT 640
 
 class Engine {
 
@@ -18,10 +23,14 @@ class Engine {
         void Events();
 
         inline bool isRunning(){return m_IsRunning;}
+        inline SDL_Renderer* GetRenderer() {return m_Renderer;}
 
     private:
         Engine(){};
         bool m_IsRunning;
+
+        SDL_Window* m_Window;
+        SDL_Renderer* m_Renderer;
         static Engine* s_Instance;
 };
 
